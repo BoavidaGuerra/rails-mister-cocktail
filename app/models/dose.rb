1244621d2be9be33225b1ca4c39e59dd04d10e26
Madalena Boavida Guerra <madalena.boavida@icloud.com>
2020-11-05 17:02:46 +0000

    Add Routes and began Controller and View for Cocktails

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,8 @@
 class Dose < ApplicationRecord
+  # Associations
   belongs_to :cocktail
   belongs_to :ingredient
-
+  # Validation
   validates :description, :ingredient, :cocktail, presence: true
+  validates_uniqueness_of :cocktail, scope: :ingredient
 end
